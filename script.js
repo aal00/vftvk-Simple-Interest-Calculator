d693b0a37d09bcf6fd24456106de02618ea55deb
aal00 <94946559+aal00@users.noreply.github.com>
2022-02-14 15:04:03 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+	
+	if ((p < 0) || (p == '')) {
+		alert('principal should be greater than 0');
+		return;
+		
+	}
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     res = document.getElementById("result").value;
@@ -8,7 +14,7 @@ function compute()
 
     year  = new Date().getFullYear();
     d = parseInt(year) + parseInt(y);
-    document.getElementById("result").innerHTML = 'If you deposit ' + p + '<br>at an interest rate of ' + r + ' <br/>You will receive an amout of ' + total + ' <br/>in the year ' + d;
+    document.getElementById("result").innerHTML = 'If you deposit <font style="background-color:yellow" >' + p + ',</font><br>at an interest rate of <font style="background-color:yellow" >' + r + ' %.</font><br/>You will receive an amout of <font style="background-color:yellow" >' + total + ',</font> <br/>in the year <font style="background-color:yellow" >' + d + '</font>';
     
 }
         
