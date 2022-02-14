ac8f436a59a4a945841d9ae63962fee372c283e5
aal00 <94946559+aal00@users.noreply.github.com>
2022-02-14 14:37:32 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    res = document.getElementById("result").value;
+    total = p * (0.01 * r) * y;
+
+    year  = new Date().getFullYear();
+    d = parseInt(year) + parseInt(y);
+    document.getElementById("result").innerHTML = 'If you deposit ' + p + '<br>at an interest rate of ' + r + ' <br/>You will receive an amout of ' + total + ' <br/>in the year ' + d;
     
 }
         
